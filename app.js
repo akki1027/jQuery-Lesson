6b2efbb2bbeda0ae5ed11cd8ef2ace0b869e4d35
akki1027 <akki10271995@gmail.com>
2020-02-27 19:13:57 +0900

    [Finish] 6-4

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // [6-4]
-$(document).ready(function () {
-    $('body').html('<h1>Hello jQuery!!</h1>');
+$(function(){
+	$('.box1').slideDown(function() {
+		$('.box1').css({
+			'background-color': '#0000FF',
+			'widht': '200',
+			'height': '100'
+		}).slideUp(5000);
+	});
 });
