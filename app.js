90117b0239262bb1d5e3113dd82e4c5812c311d1
akki1027 <akki10271995@gmail.com>
2020-02-29 14:35:32 +0900

    [Finish] 6-6

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// [6-4]
-$(function(){
-	$('.box1').slideDown(function() {
-		$('.box1').css({
-			'background-color': '#0000FF',
-			'widht': '200',
-			'height': '100'
-		}).slideUp(5000);
-	});
-});
-
 // [6-6]
+$(function(){
+    $('.box1').on('click', function(){
+        $('.box1').addClass('box1-ext');
+    });
+
+    $('.box1').mouseout(function(){
+    	$('.box1').removeClass('box1-ext');
+    });
+});
